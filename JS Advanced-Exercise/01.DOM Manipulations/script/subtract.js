f27e915d5b701ce00e9b517e1583d89e6c1493be
slavizdravkov <slavizdravkov@gmail.com>
2017-06-27 22:34:57 +0300

    Delete subtract.js

--- JS Advanced-Exercise/01.DOM Manipulations/script/subtract.js
+++
@@ -1,6 +0,0 @@
-function subtract() {
-    let numberOne = document.getElementById('firstNumber').value;
-    let numberTwo = document.getElementById('secondNumber').value;
-
-    document.getElementById('result').textContent = Number(numberOne) - Number(numberTwo);
-}
